9a0de541a81241feb180c42d99547117754f4160
Larlf.Wang <larlf.wang@gmail.com>
2015-05-25 20:22:48 +0800

    修改目录结构

---
+++ core/mm_utils.cc
@@ -0,0 +1,15 @@
+﻿#include "mirror.ui.utils.h"
+
+using namespace mirror::ui;
+
+unsigned int mirror::ui::UIUtils::idIndex = 0;
+
+std::string mirror::ui::UIUtils::newId()
+{
+	char buffer[10];
+	std::string id("UI_");
+	UIUtils::idIndex++;
+	sprintf_s(buffer, 10, "%d", UIUtils::idIndex);
+	id.append(buffer);
+	return id;
+}
---
+++ core/mm_utils.h
@@ -0,0 +1,46 @@
+﻿#ifndef MIRROR_UI_UTILS
+#define MIRROR_UI_UTILS
+
+#include "mirror.utils.h"
+
+
+//____________________________________________________________________________
+
+namespace mirror
+{
+	namespace ui
+	{
+		class Point
+		{
+		public:
+			long x;
+			long y;
+			Point() : x(0), y(0) {}
+			Point(long x, long y) : x(x), y(y) {}
+		};
+
+		class Rect
+		{
+		public:
+			long x;
+			long y;
+			long w;
+			long h;
+			Rect() : x(0), y(0), w(0), h(0) {}
+			Rect(long x, long y, long w, long h) : x(x), y(y), w(w), h(h) {}
+		};
+
+		class UIUtils
+		{
+		private:
+			static unsigned int idIndex;
+		public:
+			//生成一个新的ID
+			static std::string newId();
+		};
+	}
+}
+
+
+
+#endif
